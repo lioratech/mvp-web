e0975da387c9a8cb17ed93bea3401a4f304474bb
Giancarlo Buomprisco <giancarlopsk@gmail.com>
2025-02-20 07:40:34 +0700

    Add DialogTitle using a hidden class to prevent runtime warnings (#178)

--- packages/billing/stripe/src/components/stripe-embedded-checkout.tsx
+++ packages/billing/stripe/src/components/stripe-embedded-checkout.tsx
@@ -8,7 +8,7 @@ import {
 } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
-import { Dialog, DialogContent } from '@kit/ui/dialog';
+import { Dialog, DialogContent, DialogTitle } from '@kit/ui/dialog';
 
 import { StripeClientEnvSchema } from '../schema/stripe-client-env.schema';
 
@@ -66,6 +66,7 @@ function EmbeddedCheckoutPopup({
         onOpenAutoFocus={(e) => e.preventDefault()}
         onInteractOutside={(e) => e.preventDefault()}
       >
+        <DialogTitle className={'hidden'}>Checkout</DialogTitle>
         <div>{children}</div>
       </DialogContent>
     </Dialog>
