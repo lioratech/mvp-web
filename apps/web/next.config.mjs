af84676454586b9b4b8f0e5423a22945e202a01b
Giancarlo Buomprisco <giancarlopsk@gmail.com>
2025-04-10 09:48:51 +0700

    2.7.0 (#232)
    1. Enable Turbopack for testing builds
    2. Move turbopack configuration to new top-level config property
    3. Bump package.json version
    4. Add missing env in .env
    5. Added more ignored paths to .cursorignore

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -42,13 +42,13 @@ const config = {
     '/*': ['./content/**/*'],
   },
   redirects: getRedirects,
+  turbopack: {
+    resolveExtensions: ['.ts', '.tsx', '.js', '.jsx'],
+    resolveAlias: getModulesAliases(),
+  },
   experimental: {
     mdxRs: true,
     reactCompiler: ENABLE_REACT_COMPILER,
-    turbo: {
-      resolveExtensions: ['.ts', '.tsx', '.js', '.jsx'],
-      resolveAlias: getModulesAliases(),
-    },
     optimizePackageImports: [
       'recharts',
       'lucide-react',
@@ -116,7 +116,7 @@ async function getRedirects() {
  * @returns {Record<string, string>}
  */
 function getModulesAliases() {
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV !== 'development') {
     return {};
   }
 
