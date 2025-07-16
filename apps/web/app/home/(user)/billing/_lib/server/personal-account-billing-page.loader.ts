da8a3a903d3e6182676967ee0e1a14dd357f72b1
Giancarlo Buomprisco <giancarlopsk@gmail.com>
2025-07-16 16:11:55 +0700

    Update billing page data handling and version bump to 2.12.0 (#300)
    * Update billing page data handling and version bump to 2.12.0
    
    - Refactored billing page components to streamline data loading for subscriptions and orders.
    - Introduced `getProductPlan` function to encapsulate product plan resolution logic.
    - Updated `package.json` version from 2.11.0 to 2.12.0.

--- apps/web/app/home/(user)/billing/_lib/server/personal-account-billing-page.loader.ts
+++ apps/web/app/home/(user)/billing/_lib/server/personal-account-billing-page.loader.ts
@@ -2,25 +2,8 @@ import 'server-only';
 
 import { cache } from 'react';
 
-import { z } from 'zod';
-
 import { createAccountsApi } from '@kit/accounts/api';
 import { getSupabaseServerClient } from '@kit/supabase/server-client';
-
-/**
- * The variable BILLING_MODE represents the billing mode for a service. It can
- * have either the value 'subscription' or 'one-time'. If not provided, the default
- * value is 'subscription'. The value can be overridden by the environment variable
- * BILLING_MODE.
- *
- * If the value is 'subscription', we fetch the subscription data for the user.
- * If the value is 'one-time', we fetch the orders data for the user.
- * if none of these suits your needs, please override the below function.
- */
-const BILLING_MODE = z
-  .enum(['subscription', 'one-time'])
-  .default('subscription')
-  .parse(process.env.BILLING_MODE);
 
 /**
  * Load the personal account billing page data for the given user.
@@ -36,12 +19,9 @@ function personalAccountBillingPageDataLoader(userId: string) {
   const client = getSupabaseServerClient();
   const api = createAccountsApi(client);
 
-  const data =
-    BILLING_MODE === 'subscription'
-      ? api.getSubscription(userId)
-      : api.getOrder(userId);
-
+  const subscription = api.getSubscription(userId);
+  const order = api.getOrder(userId);
   const customerId = api.getCustomerId(userId);
 
-  return Promise.all([data, customerId]);
+  return Promise.all([subscription, order, customerId]);
 }
